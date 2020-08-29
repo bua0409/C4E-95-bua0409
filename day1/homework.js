0152ccb80a256c4d9909ae8dfb12122a62fe4561
unknown <hung8adtdk15@gmail.com>
2020-08-29 19:53:20 +0700

    day1

---
+++ day1/homework.js
@@ -0,0 +1,25 @@
+var message = 'Coding is great';
+console.log(message);
+var message = 'Coding might not be easy, but still great';
+console.log(message);
+var message = message.toLowerCase();
+console.log(message);
+var studentCount = 0;
+console.log(studentCount);
+var studentCount = 16;
+console.log(studentCount)
+var studentCount = 1;
+console.log(studentCount);
+const firstName = prompt('Enter your first name is');
+const lastName = prompt('Enter your last name is');
+alert('Hi!!!' + ' ' + lastName + firstName)
+let length = prompt('Enter side length of the square');
+let square = length **2;
+alert('the square is' + ' ' + square);
+let radius = prompt('Enter side radius of the circle');
+let circle = (length **2) *3.14;
+alert('the circle area is' + ' ' + circle);
+let tem = prompt('Enter temperature');
+let f = (tem *9/5)+32;
+alert(tem+ '(C)' + '='+ f + '(F)');
+alert('thank you!!!!!!!!')
---
+++ day1/index.js
@@ -0,0 +1,11 @@
+let number = 10;
+const myAge = 17;
+const myName = 'Hung';
+
+console.log('dfdsfaf');
+console.log(myAge);
+alert('hello world');
+const firstName = prompt('your first name???');
+console.log('your first name' +' ' + firstName);
+alert('your first name' +' ' + firstName);
+    
